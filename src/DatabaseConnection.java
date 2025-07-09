5b3f8a334bf9445c3cb3b9328df76cc3fd7623c0
kalpana-infinity <painulyk920@gmail.com>
2025-07-09 21:46:14 +0530

    DatabaseConnection.java

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String USER = "project";
+    private static final String PASSWORD = "123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
